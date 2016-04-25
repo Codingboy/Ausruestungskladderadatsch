41f7642abc023c8aa0f2e38887c770a6ef4132af
Stefan V <volzstefan97@googlemail.com>
2016-04-25 19:46:47 +0200

    #rus_emr.sqf #rus_emr.hpp

--- SV/rus_emr.hpp
+++ SV/rus_emr.hpp
@@ -1,11 +1,11 @@
-class CODI_LO_unarmed_usmc_wood : B_Soldier_F
+class CODI_LO_unarmed_rus_emr : B_Soldier_F
 {
 	side = 1;//0=opfor, 1=blufor, 2=independent, 3=civilian
 	scope = 2;
 	vehicleClass = "CODI_Infantry";
 	author = "Coding";//author
-	displayName = "unarmed";//anzeigenahme
-	faction = "CODI_LO_usmc_wood";//faction
+	displayName = "unarmed";//anzeigename
+	faction = "CODI_LO_rus_emr";//faction
 	weapons[] = {"Throw","Put"};
 	respawnWeapons[] = {"Throw","Put"};
 	items[] = {};
@@ -16,131 +16,119 @@ class CODI_LO_unarmed_usmc_wood : B_Soldier_F
 	respawnLinkedItems[] = {};
 	uniformClass = "U_BasicBody";
 };
-class CODI_LO_rflm_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_rflm_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "rflm";
 };
-class CODI_LO_cfr_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_cfr_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "cfr";
 };
-class CODI_LO_medic_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_medic_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "medic";
 };
-class CODI_LO_ftl_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_ftl_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "ftl";
 };
-class CODI_LO_lmg_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_lmg_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "lmg";
 };
-class CODI_LO_mmg_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_mmg_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "mmg";
 };
-class CODI_LO_hmg_usmc_wood : CODI_LO_unarmed_usmc_wood
-{
-	displayName = "hmg";
-};
-class CODI_LO_grn_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_grn_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "grn";
 };
-class CODI_LO_eng_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_eng_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "eng";
 };
-class CODI_LO_uav_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_uav_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "uav";
 };
-class CODI_LO_es_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_es_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "es";
 };
-class CODI_LO_dm_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_dm_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "dm";
 };
-class CODI_LO_crew_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_crew_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "crew";
 };
-class CODI_LO_com_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_com_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "com";
 };
-class CODI_LO_sl_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_sl_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "sl";
 };
-class CODI_LO_cq_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_cq_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "cq";
 };
-class CODI_LO_mortar_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_mortar_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "mortar";
 };
-class CODI_LO_amortar_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_amortar_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "amortar";
 };
-class CODI_LO_pilot_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_pilot_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "pilot";
 };
-class CODI_LO_helipilot_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_helipilot_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "helipilot";
 };
-class CODI_LO_helicrew_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_helicrew_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "helicrew";
 };
-class CODI_LO_ac_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_ac_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "ac";
 };
-class CODI_LO_lat_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_lat_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "lat";
 };
-class CODI_LO_at_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_at_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "at";
 };
-class CODI_LO_aa_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_aa_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "aa";
 };
-class CODI_LO_sniper_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_sniper_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "sniper";
 };
-class CODI_LO_flanker_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_flanker_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "flanker";
 };
-class CODI_LO_spotter_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_spotter_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "spotter";
 };
-class CODI_LO_sp_usmc_wood : CODI_LO_unarmed_usmc_wood
-{
-	displayName = "sp";
-};
-class CODI_LO_pl_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_pl_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "pl";
 };
-class CODI_LO_ps_usmc_wood : CODI_LO_unarmed_usmc_wood
+class CODI_LO_ps_rus_emr : CODI_LO_unarmed_rus_emr
 {
 	displayName = "ps";
-};
-class CODI_LO_fac_usmc_wood : CODI_LO_unarmed_usmc_wood
-{
-	displayName = "fac";
 };
